d183faa7f4ff838421a2d3bc57fe6d827c21c269
amirMshakouri <145334025+amirMshakouri@users.noreply.github.com>
2024-03-24 08:35:27 +0330

    Add files via upload

---
+++ tamrinnomre.py
@@ -0,0 +1,16 @@
+
+x = str(input("enter your grade alphabet : "))
+
+if x == "a" or x == "A" :
+    print("your grade is between 17 and 20")
+elif x == "b" or x == "B":
+    print("your grade is between 15 and 17")
+elif x == "c" or x == "C":
+    print("your grade is between 12 and 15")
+else:
+    print("your score is under 12")
+
+
+
+
+    
